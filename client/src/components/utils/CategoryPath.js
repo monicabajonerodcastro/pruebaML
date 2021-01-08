8b0ec7e521ef6d1740c532467467dd1f4f02898d
Monica Bajonero Dcastro <monicabadcas@hotmail.es>
2021-01-08 11:29:39 -0500

    Readme and bug fixes

--- client/src/components/utils/CategoryPath.js
+++ client/src/components/utils/CategoryPath.js
@@ -10,10 +10,12 @@
 
 export default function CategoryPathRender({categoryPath}){
     var breadcrumb = [];
-    categoryPath.map((category, _index) => {
-        breadcrumb.push(category.name);
-        breadcrumb.push(">");
-    });
-    breadcrumb.splice(-1,1);
+    if(categoryPath){
+        categoryPath.map((category, _index) => {
+            breadcrumb.push(category.name);
+            breadcrumb.push(">");
+        });
+        breadcrumb.splice(-1,1);
+    }
     return <span>{breadcrumb.join(" ")}</span>;
 }
